ef9272c7fb3bf138bc115661550d47bc7dc6fbf6
Maxwell Brown <brown308@msu.edu>
2014-02-06 16:18:46 -0500

    hw4

--- server.py
+++ server.py
@@ -2,22 +2,123 @@
 import random
 import socket
 import time
-import urlparse
+from urlparse import urlparse, parse_qs
+
+## for the new POST request handling
+from StringIO import StringIO
+import cgi
+
+## for the templates
+import jinja2
+import os
+
+
+##
+## HANDLE CONNECTION DEFINITION
+##
+def handle_connection(conn):
     
+    # Start reading in data from the connection
+    read = conn.recv(1)
+    while read[-4:] != '\r\n\r\n':
+	read += conn.recv(1)
+	
+    if read[-4:] == '\r\n\r\n':
+	print "READ IS DONE!"
+	
+    # Parse headers
+    request, data = read.split('\r\n',1)
+    print read
+
+    headers = {}
+    for line in data.split('\r\n')[:-2]:
+	k, v = line.split(': ',1)
+	headers[k.lower()] = v
+	
+    # Parse path and related info
+    path = urlparse(request.split(' ', 3)[1])
+    
+    # corrospond requests to .html files
+    response = {
+		'/'		: 'index.html',		\
+		'/content'	: 'content.html',	\
+		'/file'		: 'file.html',		\
+		'/image'	: 'image.html',		\
+		'/form'		: 'form.html',		\
+		'/submit'	: 'submit.html',	\
+		}
+    
+    # setup and run jinja2 templates
+    loader = jinja2.FileSystemLoader('./templates')
+    env = jinja2.Environment(loader=loader)
+    response_header =	'HTTP/1.0 200 OK\r\n' + \
+			'Content-type: text/html\r\n\r\n'
+    content = ''
+    
+    # path[4] corrosponds to field=value, and parse_qs handles that
+    values = parse_qs(path[4])
+    
+    if request.startswith('POST '):
+	while len(content) < int(headers['content-length']):
+	    content += conn.recv(1)
+    
+    environ = {'REQUEST_METHOD' : 'POST'}
+    form = cgi.FieldStorage(fp=StringIO(content), headers=headers, environ=environ)
+    
+    print 'form made!'
+    
+    # add the newly found values into the parse_qs object
+    new_values = {}
+    for key in form.keys():
+	new_values[key] = form[key].value
+
+    values.update(new_values)
+    
+    print 'values updated!'
+    
+    # Try to connect to requested page
+    if path[2] in response:
+	template = env.get_template(response[path[2]])
+    else:
+	values['page'] = path[2]
+	response_header = 'HTTP/1.0 404 Not Found\r\n\r\n'
+	template = env.get_template('404.html')
+    
+    print 'tried '+path[2]
+    
+    # use jinja2's render to get the return string
+    response_content = template.render(values)   
+    
+    # send response_header to client
+    conn.send(response_header)
+    # send response_content (aka, html) to client
+    conn.send(response_content)
+    
+    print 'conn sent!'
+    
+    # close the connection
+    conn.close()
+    
+    print 'conn closed!'
+	
+
+
+
+
+##
+## MAIN FUNCTION DEFINITION
+##
+
 def main():
     s = socket.socket()         # Create a socket object
-
-    host = socket.getfqdn() # Get local machine name
+    host = socket.getfqdn()     # Get local machine name
     port = random.randint(8000, 9999)
-    
     s.bind((host, port))        # Bind to the port
-
     print 'Starting server on', host, port
     print 'The Web server URL for this would be http://%s:%d/' % (host, port)
-
     s.listen(5)                 # Now wait for client connection.
-
     print 'Entering infinite loop; hit CTRL-C to exit'
+    
     while True:
 
         # Ctrl+C KeyboardInterrupt error handler
@@ -25,8 +126,6 @@ def main():
             # Establish connection with client.
             c, (client_host, client_port) = s.accept()
             print 'Got connection from', client_host, client_port
-
-
             # handle connection to serve page
             handle_connection(c)
 
@@ -34,185 +133,6 @@ def main():
         except (KeyboardInterrupt):
             print "\r\nEnding server.py ...\r\n"
             exit(0)
-
-
-##
-## THESE FUNCTIONS RETURN PAGE HTML CONTENT 
-##
-
-            
-def index(values):
-    return_str = '<h1>Hello, world.</h1>' + \
-                 'This is MaxwellGBrown\'s Web server.' + \
-                 '<ul>' + \
-                 '<li><a href="/content">content</a></li>' + \
-                 '<li><a href="/file">file</a></li>' + \
-                 '<li><a href="/image">image</a></li>' + \
-                 '<li><a href="/form">form</a></li>' + \
-                 '</ul>'
-    return return_str
-
-
-def files(values):
-    return_str = '<h1>Hello, world.</h1>' + \
-                 'This is MaxwellGBrown\'s Web server.' + \
-                 '<h2>File</h2>'
-    return return_str
-
-
-def image(values):
-    return_str = '<h1>Hello, world.</h1>' + \
-                 'This is MaxwellGBrown\'s Web server.' + \
-                 '<h2>Image</h2>'
-    return return_str
-
-
-def content(values):
-    return_str = '<h1>Hello, world.</h1>' + \
-                 'This is MaxwellGBrown\'s Web server.' + \
-                 '<h2>Content</h2>'
-    return return_str
-
-def form(values):
-    return_str = '<h1>Hello, world.</h1>' + \
-                 'This is MaxwellGBrown\'s Web server.' + \
-                 '<h2>Form</h2>' +\
-                 "<form action='/submit' method='POST'>" + \
-                 "<input type='text' name='firstname'>" + \
-                 "<input type='text' name='lastname'>" + \
-                 "<input type='submit' value='Submit'>" + \
-                 "</form>"
-    return return_str
-
-def submit(values):
-    
-    names = values
-
-##    now happens before passing values into function        
-##    names = (values.rsplit('&')[0].rsplit('=')[1],\
-##             values.rsplit('&')[1].rsplit('=')[1])
-    
-    return_str = '<h1>Hello, %s ' %(names["firstname"]) + \
-                 '%s.</h1>' % (names["lastname"]) + \
-                 'This is MaxwellGBrown\'s Web server.' + \
-                 '<h2>Submit</h2>'
-                 
-    return return_str
-
-
-
-
-
-
-##
-## A function that parses the values from a URL field as a dict
-##
-def split_values(values):
-    '''Takes values from URL and returns a dict with field:value qualities'''
-    return_dict = dict()
-
-    if values == '': return return_dict # handle no value error case
-    
-    for item in values.rsplit('&'):
-        return_dict[item.rsplit('=')[0]] = item.rsplit('=')[1]
-
-    return return_dict
-
-
-
-
-
-
-
-##
-## Parses connection object for path and refers it to correct function
-##
-def handle_connection(conn):
-    
-    ## handle request data
-    request_data = conn.recv(1000)
-    print request_data
-    request_type = request_data.splitlines()[0].rsplit(' ')[0] # prints the page requested (1st line, 1st word)
-    page_request = request_data.splitlines()[0].rsplit(' ')[1] # prints the page requested (1st line, 2nd word)
-
-    print request_type + ' ' + page_request
-    
-    parsed_url = urlparse.urlparse(page_request)
-
-    
-    ## initialize html_text
-    html_text = str()
-
-    ## set http_response
-    ## don't send the http_response just yet, incase page doesn't exist
-    http_response =  'HTTP/1.0 200 OK\r\n'
-    content_type = 'Content-type: text/html\r\n'
-
-
-
-    ## handle different types of HTML requests
-    if request_type == "GET":
-        ## serve HTML content
-
-        ## Split the values passed in the url as dict to pass
-        values = split_values(parsed_url.query)
-
-        
-        if parsed_url.path == '/':
-            ## cse.msu.edu:xxxx/
-            html_text = index(values)
-            
-        elif parsed_url.path == '/content':
-            ## cse.msu.edu:xxxx/content
-            html_text = content(values)
-            
-        elif parsed_url.path == '/file':
-            ## cse.msu.edu:xxxx/file
-            html_text = files(values)
-            
-        elif parsed_url.path == '/image':
-            ## cse.msu.edu:xxxx/image
-            html_text = image(values)
-
-        elif parsed_url.path == '/form':
-            ## cse.msu.edu:xxxx/form
-            html_text = form(values)
-
-        elif parsed_url.path == '/submit':
-            ## cse.msu.edu:xxxx/submit?firstname=value1&lastname=value2
-            ## only with GET
-            
-            html_text = submit(values)
-        
-
-
-    elif request_type == "POST":
-        ## POST request handler
-
-        ## Split the values passed in the HTML request as dict to pass
-        values = split_values(request_data.splitlines()[-1])
-        
-        if parsed_url.path == '/submit':
-            ## cse.msu.edu:xxxx/submit
-            ## with POST
-            html_text = submit(values)
-            
-        else:
-            html_text = '<h1>Hello, world.</h1>' + \
-                        'MaxwellGBrown\'s POST request HTML.'
-
-
-
-    
-    ## send http response and content
-    conn.send(http_response)
-    conn.send(content_type)
-    conn.send('\r\n')
-    conn.send(html_text)
-    conn.close()
-
-
-
 
 
 
--- test_server.py
+++ test_server.py
@@ -10,6 +10,8 @@ class FakeConnection(object):
         self.sent = ""
         self.is_closed = False
 
+        
+
     def recv(self, n):
         if n > len(self.to_recv):
             r = self.to_recv
@@ -24,131 +26,103 @@ class FakeConnection(object):
 
     def close(self):
         self.is_closed = True
-
-# Test a basic GET call.
+        
 
 def test_handle_connection_index():
     conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is MaxwellGBrown\'s Web server.' + \
-                      '<ul>' + \
-                      '<li><a href="/content">content</a></li>' + \
-                      '<li><a href="/file">file</a></li>' + \
-                      '<li><a href="/image">image</a></li>' + \
-                      '<li><a href="/form">form</a></li>' + \
-                      '</ul>'
+                      'Content-type: text/html\r\n\r\n'
 
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert conn.sent.startswith(expected_return), 'Got: %s' % (repr(conn.sent),)
 
 
 def test_handle_connection_file():
     conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is MaxwellGBrown\'s Web server.' + \
-                      '<h2>File</h2>'
+                      'Content-type: text/html\r\n\r\n'
 
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert conn.sent.startswith(expected_return), 'Got: %s' % (repr(conn.sent),)
 
 def test_handle_connection_content():
     conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is MaxwellGBrown\'s Web server.' + \
-                      '<h2>Content</h2>'
+                      'Content-type: text/html\r\n\r\n'
 
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert conn.sent.startswith(expected_return), 'Got: %s' % (repr(conn.sent),)
 
 def test_handle_connection_image():
     conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is MaxwellGBrown\'s Web server.' + \
-                      '<h2>Image</h2>'
+                      'Content-type: text/html\r\n\r\n'
 
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)                              
+    assert conn.sent.startswith(expected_return), 'Got: %s' % (repr(conn.sent),)                             
 
 def test_handle_connection_form():
     conn = FakeConnection("GET /form HTTP/1.0\r\n\r\n")
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                          'Content-type: text/html\r\n' + \
-                          '\r\n' + \
-                          '<h1>Hello, world.</h1>' + \
-                          'This is MaxwellGBrown\'s Web server.' + \
-                          '<h2>Form</h2>' +\
-                          "<form action='/submit' method='POST'>" + \
-                          "<input type='text' name='firstname'>" + \
-                          "<input type='text' name='lastname'>" + \
-                          "<input type='submit' value='Submit'>" + \
-                          "</form>"
+                      'Content-type: text/html\r\n\r\n'
 
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert conn.sent.startswith(expected_return), 'Got: %s' % (repr(conn.sent),)
 
 def test_handle_connection_submit_GET():
     firstname = "John"
     lastname = "Smith"
-    conn = FakeConnection("GET /submit?firstname=%s" % (firstname) + \
-                          "&lastname=%s HTTP/1.0\r\n\r\n" % (lastname) )
+    conn = FakeConnection("GET /submit?firstname=%s"%(firstname) + \
+                          "&lastname=%s HTTP/1.0\r\n\r\n"%(lastname) )
+                    
     expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                          'Content-type: text/html\r\n' + \
-                          '\r\n' + \
-                          '<h1>Hello, %s %s.</h1>' % (firstname, lastname) + \
-                          'This is MaxwellGBrown\'s Web server.' + \
-                          '<h2>Submit</h2>'
-                          
+                      'Content-type: text/html\r\n\r\n'
 
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
-
-
-def test_handle_connection_submit_POST():
-    firstname = "John"
-    lastname = "Smith"
-    conn = FakeConnection("POST /submit HTTP/1.0\r\n" + \
-                          "firstname=%s&lastname=%s\r\n" % (firstname,lastname))
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                          'Content-type: text/html\r\n' + \
-                          '\r\n' + \
-                          '<h1>Hello, %s %s.</h1>' % (firstname, lastname) + \
-                          'This is MaxwellGBrown\'s Web server.' + \
-                          '<h2>Submit</h2>'
+    assert conn.sent.startswith(expected_return), 'Got: %s' % (repr(conn.sent),)
                           
 
-    server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
-
-
-def test_handle_connection_POST():
-    conn = FakeConnection("POST / HTTP/1.0\r\n\r\n")
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                          'Content-type: text/html\r\n' + \
-                          '\r\n' + \
-                          '<h1>Hello, world.</h1>' + \
-                          'MaxwellGBrown\'s POST request HTML.'
+def test_handle_connection_GET_404():
+    conn = FakeConnection("GET /404 HTTP/1.0\r\n\r\n")
+    expected_return = 'HTTP/1.0 404 Not Found\r\n\r\n'
     
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert conn.sent.startswith(expected_return), 'Got: %s' % (repr(conn.sent),)
 
+
+def test_handle_connection_submit_POST_urlencoded():
+    firstname = "Joe"
+    lastname = "Schmoe"
+    urlencoded = "firstname=%s&lastname=%s\r\n" % (firstname,lastname)
+    conn = FakeConnection("POST /submit HTTP/1.0\r\n"+ \
+                          "Content-Type: application/x-www-form-urlencoded \r\n" + \
+                          "Content-Length: 29\r\n\r\n" + \
+                          "firstname=%s&lastname=%s\r\n"%(firstname,lastname) )
+                          
+                          
+    expected_return = 'HTTP/1.0 200 OK\r\n' + \
+                          'Content-type: text/html\r\n\r\n'
     
+    server.handle_connection(conn)
+
+    assert conn.sent.startswith(expected_return), 'Got: %s' % (repr(conn.sent),)
+    
+
+def test_handle_connection_POST_404():
+    conn = FakeConnection("POST /404 HTTP/1.0\r\n" + \
+                          "Content-Type: application/x-www-form-urlencoded \r\n"+\
+                          "Content-Length: 0\r\n\r\n")
+    expected_return = 'HTTP/1.0 404 Not Found\r\n'
+    
+    server.handle_connection(conn)
+
+    assert conn.sent.startswith(expected_return), 'Got: %s' % (repr(conn.sent),)
+
